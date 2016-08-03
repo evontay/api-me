36e7f5f67be93e0cfc516a900c59c7959bfe07ea
Evon <fumblies@gmail.com>
2016-08-03 16:23:05 +0800

    added externally hosted images and trying \n for line break

--- models/seed.js
+++ models/seed.js
@@ -46,28 +46,28 @@ var data = [
         'description': 'A collaborative data project for the Singapore startup community. Backend API built with Node.JS + Express. Frontend API build with HTML, CSS, Javascript, Ajax, JQuery, Algolia. A joint effort with my lovely team -- Geng Sng, Lew Li Min, and Winston Mi.',
         'github': 'https://github.com/evontay/startups-sg',
         'website': 'https://evontay.github.io/startups-sg/',
-        'image': 'startupsg1.png'          
+        'image': 'https://flic.kr/p/KMsYSr'          
       },
       {
         'title': 'Should I bike today?',
         'description': 'As a casual bike commuter, I made a simple single page app that advises on the daily weather to decide whether it is a good day for biking. Used the 24-hour weather forecast API from data.gov.sg. Built with Javascript, Ajax, Jquery, HTML, CSS.',
         'github': 'https://github.com/evontay/should-i-bike-today',
         'website': 'https://evontay.github.io/should-i-bike-today/',
-        'image': 'sibt1.png'
+        'image': 'https://flic.kr/p/JQTDXb'
       },
       {
         'title': 'Edible Plants of Singapore - Wiki',
         'description': 'Crowdsourced information about edible local plants for non-profit, Foodscape Collective. Built with Ruby on Rails, HTML, CSS.',
         'github': 'https://github.com/evontay/plants-wiki',
         'website': 'http://plants-wiki.herokuapp.com/',
-        'image': 'plantswiki1.png'
+        'image': 'https://flic.kr/p/KCaesA'
       },
       {
-        'title': 'CURIO MORSE: %odThe Decoding Game',
+        'title': 'CURIO MORSE: \nThe Decoding Game',
         'description': 'A quiz game to help people learn Morse Code through audio and visual signals. Built with Javascript, Jquery, HTML, CSS.',
         'github': 'https://github.com/evontay/morse-game',
         'website': 'https://evontay.github.io/morse-game/',
-        'image': 'morse1.png'
+        'image': 'https://flic.kr/p/KMt1Tk'
       }
     ]
   },
